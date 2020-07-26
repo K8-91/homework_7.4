4ab344179508404e7ed1e0abe7c549cdcc86871a
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-07-26 12:46:45 +0200

    Corrections

--- 7.4_zad1.py
+++ 7.4_zad1.py
@@ -1,7 +1,7 @@
 import random
 from operator import attrgetter
 
-library=[]\
+library = []
 
 class Movie:
     def __init__(self, tittle, publ_year, genre, views_number):
@@ -26,33 +26,33 @@ class Series (Movie):
     
     def __str__(self):
         if self.season <10:
-            if self.episode <10:
-                return f'{self.tittle} S0{self.season}E0{self.episode}'
-            else:
-                return f'{self.tittle} S0{self.season}E{self.episode}'
+            season_no = f'S0{self.season}'
         else:
-            if self.episode <10:
-                return f'{self.tittle} S{self.season}E0{self.episode}'
-            else:
-                return f'{self.tittle} S{self.season}E{self.episode}'
+            season_no = f'S0{self.season}'
+
+        if self.episode <10:
+            episode_no = f'E0{self.episode}'
+        else:
+            episode_no = f'E{self.episode}'
+        return f'{self.tittle} {season_no} {episode_no}'
         
     def __repr__(self):
         return f'{self.tittle} {self.publ_year} {self.genre} {self.episode} {self.season} {self.views_number}'
 
-S1 = Series(tittle= 'Friends', publ_year= 1994, genre='comedy', episode=3, season=10, views_number=374)
-F1 = Movie (tittle = 'Lord of Rings', publ_year = 2003, genre = 'fantasy', views_number= 700)
-S2 = Series(tittle= 'Stranger Things', publ_year= 2019, genre='fantasy', episode=5, season=3, views_number=255)
-F2 = Movie (tittle = 'We re the Millers', publ_year = 2003, genre = 'comedy', views_number= 500)
-S3 = Series(tittle= 'Broadchurch', publ_year= 2013, genre='drama', episode=13, season=1, views_number=197)
-F3 = Movie (tittle = 'Django', publ_year = 2012, genre = 'western', views_number= 648) 
-library.append(S1)
-library.append(F1)
-library.append(S2)
-library.append(F2)
-library.append(S3)
-library.append(F3)
-movie_list=[]
-series_list=[]
+serial_1 = Series(tittle = 'Friends', publ_year = 1994, genre ='comedy', episode =3, season = 10, views_number = 374)
+film_1 = Movie (tittle = 'Lord of Rings', publ_year = 2003, genre = 'fantasy', views_number = 700)
+serial_2 = Series(tittle = 'Stranger Things', publ_year = 2019, genre='fantasy', episode = 5, season = 3, views_number = 255)
+fiilm_2 = Movie (tittle = 'We re the Millers', publ_year = 2003, genre = 'comedy', views_number = 500)
+serial_3 = Series(tittle = 'Broadchurch', publ_year = 2013, genre = 'drama', episode=13, season=1, views_number = 197)
+film_3 = Movie (tittle = 'Django', publ_year = 2012, genre = 'western', views_number = 648) 
+library.append(serial_1)
+library.append(film_1)
+library.append(serial_2)
+library.append(fiilm_2)
+library.append(serial_3)
+library.append(film_3)
+movie_list = []
+series_list = []
 
 def get_movies():
     for position in library:
@@ -60,7 +60,7 @@ def get_movies():
             movie_list.append(position)
         else:
             pass
-    sorted_movie_list = sorted(movie_list, key=attrgetter('tittle'))
+    sorted_movie_list = sorted(movie_list, key = attrgetter('tittle'))
     return sorted_movie_list
 
 def get_series():
@@ -79,7 +79,7 @@ def search(give_tittle):
 
 def generate_views():
     element = random.choice(library)
-    element.views_number+=random.randint(0,100)
+    element.views_number += random.randint(0,100)
     return (element.tittle, element.views_number)
 
 def generate_views_10():
@@ -87,7 +87,7 @@ def generate_views_10():
         print(generate_views()) #kiedy uzywam "return" nie zwraca mi funkcji 10 razy, tylko raz. Jednak z funkcja print na koniec printuje "None"
 
 def top_tittle(top):
-    sorted_library = sorted(library, key = attrgetter('views_number'), reverse= True)
+    sorted_library = sorted(library, key = attrgetter('views_number'), reverse = True)
     return sorted_library[0:top]
 
 #check
